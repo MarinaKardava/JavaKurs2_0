4ac741a517df9ee302fcdf41ed3e91524084a79c
Marina Kardava <MarKardava@yandex.ru>
2019-12-06 20:53:25 +0300

    Задание №4_1

---
+++ Task4_1.java
@@ -0,0 +1,50 @@
+/*Массив размерностью 20, заполняется случайными целыми числами от -10 до 10.
+Найти максимальный отрицательный и минимальный положительный элементы массива. Поменять их местами.*/
+
+
+import java.util.*;
+
+public class Task4_1 {
+    public static void main(String[] args) {
+        int n =10;
+        int arr[] =  new int[n];
+        Scanner in =  new Scanner( System.in);
+        for( int i= 0; i< n; i++){
+            System.out.println("Введите чисо "+i+": ");
+            arr [i] = in.nextInt();
+        }
+        System.out.print("Введенный массив: ");
+        for( int i= 0; i< n; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        int max =  arr[0], min =  arr[0];
+        int indexMax = 0, indexMin = 0;
+        for( int i =  0; i < n; i++) {
+            if (arr[i] > max){
+                max = arr[i];
+                indexMax = i;
+            }
+            if (arr[i] < min){
+                min = arr[i];
+                indexMin = i;
+            }
+        }
+        System.out.println("\nМаксималное число: " + max +"\nМинимальное число: " + min);
+
+        System.out.print("Полученный массив: ");
+        for( int i= 0; i < n; i++){
+            if (arr[i] == max) {
+                arr [i] = min;
+                min = max;
+                max = arr[i];
+            }
+            if (arr[i] == min) {
+                arr[i] = max;
+                max = min;
+                min = arr [i];
+            }
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
